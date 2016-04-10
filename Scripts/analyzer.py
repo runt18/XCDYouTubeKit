39ba5c418fead1c9f98a27d1626b5d211a30c578
Cody <cody@quantifiedcode.com>
2016-04-10 00:23:26 +0000

    Migrated `%` string formating

--- Scripts/analyzer.py
+++ Scripts/analyzer.py
@@ -8,7 +8,7 @@ if not run_clang_static_analyzer[:1] in 'YyTt123456789':
 
 analyzer_results_dir = os.path.join(os.environ['TARGET_TEMP_DIR'], 'StaticAnalyzer', os.environ['PROJECT_NAME'], os.environ['TARGET_NAME'], os.environ['CURRENT_VARIANT'], os.environ['CURRENT_ARCH'])
 if not os.path.exists(analyzer_results_dir):
-	sys.exit("error: Static Anaylzer results not found, expected in %s" % analyzer_results_dir)
+	sys.exit("error: Static Anaylzer results not found, expected in {0!s}".format(analyzer_results_dir))
 
 exit_code = 0
 for result in glob.iglob(os.path.join(analyzer_results_dir, "*.plist")):
@@ -17,7 +17,7 @@ for result in glob.iglob(os.path.join(analyzer_results_dir, "*.plist")):
 		for diagnostic in plist['diagnostics']:
 			location = diagnostic['location']
 			path = plist['files'][location['file']]
-			print "%s:%s:%s: error: Static Anaylzer Issue: %s" % (path, location['line'], location['col'], diagnostic['description'])
+			print "{0!s}:{1!s}:{2!s}: error: Static Anaylzer Issue: {3!s}".format(path, location['line'], location['col'], diagnostic['description'])
 			exit_code = 1
 
 sys.exit(exit_code)
